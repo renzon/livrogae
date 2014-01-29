07fc543a0b5dc41b4816e8cc9a91ac6e4c7cc222
renzon <renzo@gmail.com>
2014-01-29 18:14:32 -0200

    Olá Webapp2

--- main.py
+++ main.py
@@ -2,12 +2,15 @@
 from __future__ import absolute_import, unicode_literals
 import webapp2
 
+
 class HomeHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Olá Mundo!')
 
 
+class OutroHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Olá Wepapp2!')
 
-app = webapp2.WSGIApplication([
-    ('/', HomeHandler)
-], debug=True)
+
+app = webapp2.WSGIApplication([('/', HomeHandler),('/outra', OutroHandler)], debug=True)
