05d9158223fe8c4d20ba9418693809a3690bd671
renzon <renzo@gmail.com>
2014-02-11 00:39:00 -0200

    Método redirecionar com url hardcoded

--- backend/src/web/usuario.py
+++ backend/src/web/usuario.py
@@ -6,6 +6,11 @@ def index(_resp):
     _resp.write("Página do Usuário")
 
 
+def redirecionar(_handler):
+    url=r'/usuario/ola/Renzo/Nuccitelli'
+    _handler.redirect(url)
+
+
 def ola(_resp, nome, sobrenome):
     _resp.write("Olá %s %s" % (nome, sobrenome))
 
