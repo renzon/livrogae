d2b279fd719b7d4408cbe4a59b7e704b078aa183
renzon <renzo@gmail.com>
2014-02-17 20:17:48 -0300

    Instalação de arquivos estáticos do Twitter Bootstrap

--- backend/src/web/home.py
+++ backend/src/web/home.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from web import my_form
 from tekton import router
 
 
 def index(_write_tmpl):
-    url = router.to_path(my_form)
-    _write_tmpl('templates/home.html', {'form_url': url})
+    _write_tmpl('templates/index.html')
