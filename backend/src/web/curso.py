f94ef8135283c8089f1fcc3ff86273a5624abdf0
renzon <renzo@gmail.com>
2014-02-18 15:58:48 -0300

    acesso a parâmetros Python por template Jinja

--- backend/src/web/curso.py
+++ backend/src/web/curso.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
+
 def index(_write_tmpl):
-    _write_tmpl('/templates/curso_home.html')
+    class Curso(object):
+        def __init__(self, nome=''):
+            self.nome = nome
+
+    cursos = [Curso(nome) for nome in ('PyPrático',
+                                       'Objetos Pythônicos',
+                                       'Python para quem sabe Python')]
+
+    dct = {'lista_cursos': cursos}
+    _write_tmpl('/templates/curso_home.html', dct)
