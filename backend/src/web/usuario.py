dc59de74487536061f87f22a678db9db1d2abe1c
renzon <renzo@gmail.com>
2014-03-01 13:14:57 -0300

    Interface de usuários

--- backend/src/web/usuario.py
+++ backend/src/web/usuario.py
@@ -3,15 +3,8 @@ from __future__ import absolute_import, unicode_literals
 from tekton import router
 
 
-def index(_resp):
-    _resp.write("Página do Usuário")
+def index(_write_tmpl):
+    _write_tmpl("/templates/usuario_home.html")
 
 
-def redirecionar(_handler):
-    url = router.to_path(ola, 'Renzo', 'Nuccitelli')
-    _handler.redirect(url)
-
-
-def ola(_resp, nome, sobrenome):
-    _resp.write("Olá %s %s" % (nome, sobrenome))
 
