7241c3b37c1bd8f26abc2359f349588812778155
renzon <renzo@gmail.com>
2014-03-10 19:16:12 -0300

    Salvamento de curso

--- backend/src/web/curso.py
+++ backend/src/web/curso.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from curso.model import Curso
 from tekton import router
 
 
-class Curso(object):
+class CursoFake(object):
     def __init__(self, nome='', slug=''):
         self.slug = slug
         self.nome = nome
 
 
-cursos = [Curso(nome, slug) for nome, slug in (('PyPrático', 'pypratico'),
+cursos = [CursoFake(nome, slug) for nome, slug in (('PyPrático', 'pypratico'),
                                                ('Objetos Pythônicos', 'objetos-pythonicos'),
                                                ('Python para quem sabe Python', 'pythor-para-quem-sabe-python'))]
 
@@ -18,8 +19,16 @@ cursos_dct = {curso.slug: curso for curso in cursos}
 
 def index(_write_tmpl):
     dct = {'lista_cursos': cursos,
-           'matricula_url': router.to_path(matricula)}
+           'matricula_url': router.to_path(matricula),
+           'salvar_url': router.to_path(salvar)}
     _write_tmpl('/templates/curso_home.html', dct)
+
+
+def salvar(_handler, nome):
+    curso = Curso(nome=nome)
+    curso.put()
+    path = router.to_path(index)
+    _handler.redirect(path)
 
 
 def matricula(_write_tmpl, curso_slug):
