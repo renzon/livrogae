02c7080b759415c919d7e5e787e1d4efdaac03e7
renzon <renzo@gmail.com>
2014-03-20 00:59:30 -0300

    Cursor funcionando, com exceção de páginas com numero muito alto

--- backend/src/web/usuario.py
+++ backend/src/web/usuario.py
@@ -1,34 +1,74 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from google.appengine.ext.ndb.query import Cursor
 from tekton import router
 from usuario.model import UsuarioGoogle, Usuario, UsuarioFacebook
 
+BUSCA_FRENTE = 'FRENTE'
+BUSCA_TRAS = 'TRAS'
 
-def index(_write_tmpl, pagina_atual=1):
-    pagina_atual = int(pagina_atual)
+
+def index(_write_tmpl, pagina_origem=1, direcao_busca=BUSCA_FRENTE, cursor_urlsafe='None', pagina_destino=1):
+    #tranformacao e calculo de parametros
+    pagina_origem = int(pagina_origem)
+    pagina_destino = int(pagina_destino)
     TAMANHO_DA_PAGINA = 2
+    nova_direcao_busca = BUSCA_TRAS if (pagina_destino - pagina_origem) < 0 else BUSCA_FRENTE
 
-    #Busca assíncrona por usuários
-    query = Usuario.query().order(Usuario.nome)
-    offset = (pagina_atual - 1)*TAMANHO_DA_PAGINA
-    usuarios_future = query.fetch_async(offset=offset, limit=TAMANHO_DA_PAGINA)
+    if cursor_urlsafe == 'None':
+        cursor_urlsafe = None
+    cursor = Cursor(urlsafe=cursor_urlsafe)
+
+    # Funcao para definir se o usuario esta decrementando ou incrimentando a pagina
+    def direcao_pesquisa_mudou():
+        return direcao_busca != nova_direcao_busca
+
+    #Calculo do offset de acordo com mudanca de direcao na busca
+    offset = 0
+    if cursor_urlsafe is not None:
+        offset = abs(pagina_destino - pagina_origem)
+        offset = offset if direcao_pesquisa_mudou() else max(offset - 1, 0)
+        offset *= TAMANHO_DA_PAGINA
+
+    #Funcao de pesquisa de usuarios
+    def pesquisar_usuarios(query, cursor, offset):
+        if direcao_pesquisa_mudou() and cursor:
+            cursor = cursor.reversed()
+        return query.fetch_page_async(TAMANHO_DA_PAGINA,
+                                      offset=offset,
+                                      start_cursor=cursor)
+
+    #Pesquisa assincrona de usuarios de acordo com direcao da busca
+    if nova_direcao_busca == BUSCA_TRAS:
+        query = Usuario.query().order(-Usuario.nome)
+        usuarios_future = pesquisar_usuarios(query, cursor, offset)
+    else:
+        query = Usuario.query().order(Usuario.nome)
+        usuarios_future = pesquisar_usuarios(query, cursor, offset)
 
     #Setup de numeração de páginas
-    pagina_inicial = max(1, pagina_atual - 2)
+    pagina_inicial = max(1, pagina_destino - 2)
     pagina_final = pagina_inicial + 4
-    pagina_anterior = max(pagina_inicial, pagina_atual - 1)
+    pagina_anterior = max(pagina_inicial, pagina_destino - 1)
 
     #Construção de parâmetros a serem renderizados pelo template
     dct = {'adicionar_usuario_google_path': router.to_path(google),
            'adicionar_usuario_face_path': router.to_path(face),
-           'home_path': router.to_path(index),
-           'pagina_atual': pagina_atual,
+           'pagina_atual': pagina_destino,
            'pagina_inicial': pagina_inicial,
            'pagina_final': pagina_final,
            'pagina_anterior': pagina_anterior,
-           'pagina_posterior': pagina_atual + 1,
-           'usuarios': usuarios_future.get_result()}
+           'pagina_posterior': pagina_destino + 1}
 
+    usuarios, cursor, mais_resultados = usuarios_future.get_result()
+
+    # revertando resultado para manter ordem crescente na visualizacao de busca
+    if nova_direcao_busca == BUSCA_TRAS:
+        usuarios.reverse()
+
+    cursor_urlsafe = cursor.urlsafe() if cursor else 'None'
+    dct['home_path'] = router.to_path(index, pagina_destino, nova_direcao_busca, cursor_urlsafe)
+    dct['usuarios'] = usuarios
     _write_tmpl("/templates/usuario_home.html", dct)
 
 
