3be6808f86d66c877bf51805226247b8a49ca2a9
renzon <renzo@gmail.com>
2014-04-02 22:27:12 -0300

    Inclusão de middleware json

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 import tmpl_middleware
-from tekton.gae.middleware import router_middleware, parameter, webapp2_dependencies, email_errors
+from tekton.gae.middleware import router_middleware, parameter, webapp2_dependencies, email_errors, json_middleware
 
 SENDER_EMAIL = 'renzon@gmail.com'
 WEB_BASE_PACKAGE = "web"
-MIDDLEWARES = [tmpl_middleware.execute, email_errors.execute, webapp2_dependencies.execute, parameter.execute,
+MIDDLEWARES = [tmpl_middleware.execute,
+               json_middleware.execute,
+               email_errors.execute,
+               webapp2_dependencies.execute,
+               parameter.execute,
                router_middleware.execute]
 
