cf2ddae97c9850fdd6a3be324f8d3b40f96143d0
continuousdeliverybot <continuousdeliverybot@gmail.com>
2014-06-16 12:58:25 -0300

    atualizando settings para utilizar a class Middleware

--- backend/src/tmpl_middleware.py
+++ backend/src/tmpl_middleware.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from tekton.gae.middleware import Middleware
 import tmpl
 
 
-def execute(next_process, handler, dependencies, **kwargs):
-    def write_tmpl(template_name, values=None):
-        values = values or {}
-        return handler.response.write(tmpl.render(template_name, values))
+class TemplateMiddleware(Middleware):
+    def set_up(self):
+        def write_tmpl(template_name, values=None):
+            values = values or {}
+            return self.handler.response.write(tmpl.render(template_name, values))
 
-    dependencies["_write_tmpl"] = write_tmpl
-    dependencies["_render"] = tmpl.render
-    next_process(dependencies, **kwargs)
+        self.dependencies["_write_tmpl"] = write_tmpl
+        self.dependencies["_render"] = tmpl.render
